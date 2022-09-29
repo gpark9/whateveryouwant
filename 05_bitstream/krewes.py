f3b091389bd3b4ea16c0a98450a94e2c3ad97342
Gitae Park <78169596+gpark9@users.noreply.github.com>
2022-09-29 13:14:47 -0400

    Update krewes.py

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,3 +1,24 @@
+'''
+Gitae Park & Gabriel Thompson
+SoftDev
+K05 -- krewes/Making a random generator to return the name of a student given a file krewes.txt
+2022-09-29
+.3 hrs
+OP Summary for randomdevo()
+1) Read the txt file
+2) Split the txt at all "@@@" into lists of all individuals
+3) Iterated through list and split at "$$$" into lists of pd, devos, ducky
+4) Assigned Devos as key in dictionary devo and list of pd and ducky as value of each key
+5) Randomly chose a key and printed devos name, pd, and ducky name
+
+Q/C/C
+How do I make a dictionary within a dictionary?
+
+DISCO
+.split method, how to assign keys and values in a dictionary
+'''
+
+
 import random
 f = open('krewes.txt', 'r')
 content = f.read()
