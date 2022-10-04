86ddfa53bb1e8df7728fb2a596b774aa0f597a68
Gitae Park <78169596+gpark9@users.noreply.github.com>
2022-10-04 11:01:07 -0400

    Update app.py

--- 07_teardown/app.py
+++ 07_teardown/app.py
@@ -1,4 +1,10 @@
-# your heading here
+'''
+Git Bri in May: Gitae Park, May Qiu, Brianna Tieu
+SOFTDEV
+pd07
+2022-10-03
+time spent: .5 hr
+'''
 
 from flask import Flask
 
@@ -14,18 +20,22 @@ app.run()  # Q5: Where have you seen similar constructs in other languages?
 
 '''
 DISCO:
+- You have to click on the link printed in the console to open the webpage.
+- All machines that successfully run the code above must have flask installed.
 
 QCC:
-0. 
-1. 
-2. 
-3. 
-4. 
-5. 
+0. Creating an object in Java
+1. file paths, URLs
+2. It prints in the console.
+3. __main__
+4. This appears on the local webpage. We saw "No hablo queso" on the webpage
+   that appears when you click the link provided in the terminal after running
+   app.py.
+5. java
 ...
 
 INVESTIGATIVE APPROACH:
-<Your concise summary of how
- you and your team set about
- "illuminating the cave of ignorance" here...>
+- Our team realized that we needed to click the link in order to generate
+output in our console. We also installed flask on our local/home machines in
+order to run the code.
 '''
