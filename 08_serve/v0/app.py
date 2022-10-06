fb647e2b44c0e8900269ab8735dc455895451cff
Gitae Park <78169596+gpark9@users.noreply.github.com>
2022-10-06 12:51:48 -0400

    Update app.py

--- 08_serve/v0/app.py
+++ 08_serve/v0/app.py
@@ -1,14 +1,14 @@
-# Clyde 'Thluffy' Sinclair
+# Blinking Holly Fountains - Gitae Park, Ravindra Mangar, Sam Cowan
 # SoftDev
 # Oct 2022
 
 from flask import Flask
-app = Flask(__name__) # ...
+app = Flask(__name__) # I think it will create a new flask app with the name of the file
 
-@app.route("/") # ...
+@app.route("/") # I think the app will display the information of this file in the root 
 def hello_world():
-    print(__name__) # ...
-    return "No hablo queso!"  # ...
+    print(__name__) # I think the file will print the name of the file in the terminal.
+    return "No hablo queso!"  # I think the file will display "No hablo queso" to the webpage.
 
-app.run()  # ...
+app.run()  # I think the webpage will render on the localhost IP
                 
