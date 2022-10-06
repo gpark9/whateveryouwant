a210fdd295702081bbc549271bf4e713fe5750f7
Gitae Park <78169596+gpark9@users.noreply.github.com>
2022-10-06 13:00:02 -0400

    Update app.py

--- 08_serve/v2/app.py
+++ 08_serve/v2/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__) #create instance of class Flask
 @app.route("/")       #assign fxn to route
 def hello_world():
     print("about to print __name__...")
-    print(__name__)   # Where will this go? - in the terminal
+    print(__name__)   # Where will this go? - in the terminal - when you refresh the webpage
     return "No hablo queso!"
 
 app.run()
