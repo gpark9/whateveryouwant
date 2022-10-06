14a5af81d7664cc0f99bbc7e8a70caffea8e0da2
Gitae Park <78169596+gpark9@users.noreply.github.com>
2022-10-06 13:01:42 -0400

    Update app.py

--- 08_serve/v3/app.py
+++ 08_serve/v3/app.py
@@ -1,4 +1,4 @@
-# Clyde 'Thluffy' Sinclair
+# Blinking Holly Fountains - Gitae Park, Ravindra Mangar, Sam Cowan
 # SoftDev
 # Oct 2022
 
@@ -8,8 +8,8 @@ app = Flask(__name__) #create instance of class Flask
 @app.route("/")       #assign fxn to route
 def hello_world():
     print("about to print __name__...")
-    print(__name__)   #where will this go?
+    print(__name__)   #where will this go? - this will go to the terminal
     return "No hablo queso!"
 
-app.debug = True
+app.debug = True # more information in the terminal - don't need to refresh
 app.run()
