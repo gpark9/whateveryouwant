daf1a23357c25d1c6b3daaa38b8f6bf2505e03b7
Gitae Park <78169596+gpark9@users.noreply.github.com>
2022-10-06 12:56:10 -0400

    Update app.py

--- 08_serve/v1/app.py
+++ 08_serve/v1/app.py
@@ -1,4 +1,4 @@
-# Clyde 'Thluffy' Sinclair
+# Blinking Holly Fountains - Gitae Park, Ravindra Mangar, Sam Cowan
 # SoftDev
 # Oct 2022
 
@@ -7,6 +7,7 @@ app = Flask(__name__) #create instance of class Flask
 
 @app.route("/")       #assign fxn to route
 def hello_world():
+    # missing print(__name__) - it won't print in the terminal but in the webpage the result doesn't change
     return "No hablo queso!"
 
 app.run()
