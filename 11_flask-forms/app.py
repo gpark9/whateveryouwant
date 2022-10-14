6090131d593b5d9b124b1b9e6401f3afe65946a2
Gitae Park <78169596+gpark9@users.noreply.github.com>
2022-10-14 13:10:25 -0400

    Update app.py

--- 11_flask-forms/app.py
+++ 11_flask-forms/app.py
@@ -65,3 +65,4 @@ if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True 
     app.run()
+ 
