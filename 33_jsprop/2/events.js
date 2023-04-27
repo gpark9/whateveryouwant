14adad55a8807db13d045e42ea6132406ef3687f
Gitae Park <78169596+gpark9@users.noreply.github.com>
2023-04-27 18:52:03 -0400

    Update events.js

---
+++ 33_jsprop/2/events.js
@@ -0,0 +1,26 @@
+// demo 2
+// JS event propagation
+
+var tds = document.getElementsByTagName('td');
+var trs = document.getElementsByTagName('tr');
+var table = document.getElementsByTagName('table')[0];
+
+var clicky = function(e) {
+  alert( this.innerHTML );
+};
+
+for (var x=0; x < tds.length; x++) {
+  tds[x].addEventListener('click', clicky);
+}
+
+for (x=0; x < trs.length; x++) {
+  trs[x].addEventListener('click', clicky);
+}
+
+table.addEventListener('click', clicky);
+
+
+// Q: When user clicks on a cell, in what order will the pop-ups appear?
+
+// A: The contents of the current cell, then the contents of the row it
+// belongs to, and then the whole table
